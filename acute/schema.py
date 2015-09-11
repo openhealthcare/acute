1e82de4a70d768b3269aa42d5c426db29b9e732d
David Miller <david@deadpansincerity.com>
2015-09-11 20:02:21 +0100

    Remove clerking from AAU
    closes #8

--- acute/schema.py
+++ acute/schema.py
@@ -6,6 +6,15 @@ from acute import models
 list_columns = [
     models.Demographics,
     models.Location,
+    models.PastMedicalHistory,
+    models.Diagnosis,
+    models.Plan,
+    models.Rescuscitation
+]
+
+list_columns_take = [
+    models.Demographics,
+    models.Location,
     models.Clerking,
     models.PastMedicalHistory,
     models.Diagnosis,
@@ -16,7 +25,6 @@ list_columns = [
 list_columns_specialist_teams = [
     models.Demographics,
     models.Location,
-    models.Clerking,
     models.PastMedicalHistory,
     models.Diagnosis,
     models.Plan,
@@ -27,6 +35,9 @@ list_columns_specialist_teams = [
 
 list_schemas = {
     'default': list_columns,
+    'take': {
+        'default': list_columns_take
+    },
     'cardiology': {
         'default': list_columns_specialist_teams
     },
